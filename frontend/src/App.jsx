a513f9522f411372cc971a071cbb2b94f71891e1
Constance Chen <constancechen@Constances-MacBook-Pro.local>
2024-10-02 20:08:00 -0400

    commit_1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,17 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  return <>hello world</>;
+  return (
+    <div className="our_mission">
+      <h1> OUR MISSION </h1>
+      <div className="mission_text">
+        <p>Safety Straw’s mission is to eliminate the number of</p>
+        <p>date rape victims from the world. Our goal is to</p>
+        <p>create a <span className="highlight">safer</span>, more <span className="highlight">secure</span> nightlife environment.</p>
+      </div>
+      <p className="author"> ~ Jack & Zak </p>
+    </div>
+  );
 }
 
 export default App;
