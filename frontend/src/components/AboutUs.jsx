0298776c166b49dc3f7ddd8d43cdfb88c1cb7ee2
reitak97 <reita.koyama97@gmail.com>
2024-10-08 20:17:49 -0400

    created about us component

---
+++ frontend/src/components/AboutUs.jsx
@@ -0,0 +1,14 @@
+export default function AboutUs() {
+    return (
+        <div>
+        <h1>About Us</h1>
+        <p>
+            Safety Straw is a company that is dedicated to providing the best
+            possible solution to the plastic straw problem. Our straws are made of
+            100% biodegradable materials and are safe for the environment. We are
+            committed to reducing the amount of plastic waste that ends up in our
+            oceans and landfills.
+        </p>
+        </div>
+    );
+    }
