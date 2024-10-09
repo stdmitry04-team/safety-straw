83b6304bac7081f5160255b7792729296cbf1878
Dmitry <starodu5@gmail.com>
2024-10-09 17:31:14 -0400

    nav, vid, prod, mission, footer NR

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import Navbar from "./components/nav.jsx";
 import Video from "./components/video.jsx";
 import Footer from "./components/footer.jsx"
+import Background from "./components/background.jsx"
+import Mission from "./components/our_mission.jsx"
 
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
     <div class="app">
       <Navbar/>
       <Video/>
+      <Background></Background>
+      <Mission></Mission>
       <Footer/>
     </div>
 
