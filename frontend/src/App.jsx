e5aa9478ade92b562ef5df55bbcb73e40a912ad8
reitak97 <reita.koyama97@gmail.com>
2024-10-10 20:34:51 -0400

    restored app.jsx to default

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,12 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Video from "./components/video";
-
+import AboutUs from "./components/AboutUs";
 
 function App() {
   return(
-    <Video/>
+    <>Hello world
+    </>
   )
 
 }
