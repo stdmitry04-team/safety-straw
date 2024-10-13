a0376fb89b4636ce337efcf957bf4a3b0917a795
emberzhao1 <zhaoembe@msu.edu>
2024-10-13 18:15:00 -0400

    started blog posts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,24 @@
 import { useState } from "react";
 import "./App.css";
-import Navbar from "./components/nav.jsx";
-import Video from "./components/video.jsx";
-import Footer from "./components/footer.jsx"
-import Background from "./components/background.jsx"
-import Mission from "./components/our_mission.jsx"
-import Product from "./components/product.jsx"
-
+// import Navbar from "./components/nav.jsx";
+// import Video from "./components/video.jsx";
+// import Footer from "./components/footer.jsx"
+// import Background from "./components/background.jsx"
+// import Mission from "./components/our_mission.jsx"
+// import Product from "./components/product.jsx"
+import Blogpost from "./components/blogpost.jsx"
 
 
 function App() {
   return (
     <div class="app">
-      <Navbar/>
+      {/* <Navbar/>
       <Video/>
       <Product></Product>
       <Background></Background>
       <Mission></Mission>
-      <Footer/>
+      <Footer/> */}
+      <Blogpost></Blogpost>
     </div>
 
   );
---
+++ frontend/src/components/blogpost.jsx
@@ -0,0 +1,8 @@
+import "../styles/blogposts.css"
+export default function blogpost () {
+    return (
+        <div classNmae = "blog-post">
+            <p className = "blog-posts"> BLOG POSTS</p>
+        </div>
+    )
+}
