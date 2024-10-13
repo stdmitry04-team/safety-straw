decaad7653d9f795971986a90313dc484e46a2cb
emberzhao1 <zhaoembe@msu.edu>
2024-10-13 18:47:24 -0400

    editing blog post title

--- frontend/src/components/blogpost.jsx
+++ frontend/src/components/blogpost.jsx
@@ -1,7 +1,7 @@
 import "../styles/blogposts.css"
 export default function blogpost () {
     return (
-        <div classNmae = "blog-post">
+        <div className = "blog-post">
             <p className = "blog-posts"> BLOG POSTS</p>
         </div>
     )
