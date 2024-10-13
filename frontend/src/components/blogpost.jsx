8276193ec78910c677e8cdad9c0cb51bc224a861
emberzhao1 <zhaoembe@msu.edu>
2024-10-13 19:16:13 -0400

    adding images

--- frontend/src/components/blogpost.jsx
+++ frontend/src/components/blogpost.jsx
@@ -1,8 +1,16 @@
 import "../styles/blogposts.css"
+import l_image from "../images/blogimageleft.png"
+import m_image from "../images/blogimagemiddle.png"
+import r_image from "../images/blogimageright.png"
+
+
 export default function blogpost () {
     return (
         <div className = "blog-post">
             <p className = "blog-posts"> BLOG POSTS</p>
+            <img className = "blog_l_image" src={l_image} alt="" />
+            <img className = "blog_m_image" src={m_image} alt="" />
+            <img className = "blog_r_image" src={r_image} alt="" />
         </div>
     )
 }
