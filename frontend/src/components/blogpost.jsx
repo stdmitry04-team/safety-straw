2828c2958f4e5de168ea4a3e3400e5f8a1f554c7
Shiyuan Chen <chens100@msu.edu>
2024-10-13 19:19:10 -0400

    background

--- frontend/src/components/blogpost.jsx
+++ frontend/src/components/blogpost.jsx
@@ -1,7 +1,9 @@
 import "../styles/blogposts.css"
+import background from "../assets/blogpost_bg.svg"
 export default function blogpost () {
     return (
         <div className = "blog-post">
+            <img className = "blogpost-background" src={background} alt="" />
             <p className = "blog-posts"> BLOG POSTS</p>
         </div>
     )
