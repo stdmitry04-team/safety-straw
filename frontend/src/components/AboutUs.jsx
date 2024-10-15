114f88764016ffe4f29eeebe239ff98237a63325
reitak97 <reita.koyama97@gmail.com>
2024-10-14 22:06:37 -0400

    added responsive design

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -1,40 +1,34 @@
 import "../styles/AboutUs.css";
 export default function AboutUs() {
     return (
-        <div className="container">
-            <div className="top">
-                <div className="about-us">
-                    <h1>
-                        ABOUT US
-                    </h1>
-                </div>
-                
-            </div>
-            <div className="bottom">
-                <div className="left">
-                    <iframe src="https://www.youtube.com/embed/hC4Tc3nyhLg"></iframe>
-                </div>
-                <div className="right">
-                    <div className="text">
-                        <p>
-                            We're Jack & Zak, childhood friends and co-founders of Safety Straw
-                        </p>
-                        <br></br>
-                        <p>
-                            The company was born to create a safer environment for all and make a real difference. Want to find out more about our story?
-                        </p>
+        <div className="about-us">
+            <div className="container">
+                <div className="top">
+                    <div className="header">
+                        <h1>
+                            ABOUT US
+                        </h1>
                     </div>
-                    <div className="contact">
-                        <button>Contact Us</button>
-
-                    </div>
-
                 </div>
-                
-
+                <div className="bottom">
+                    <div className="left">
+                        <iframe src="https://www.youtube.com/embed/hC4Tc3nyhLg"></iframe>
+                    </div>
+                    <div className="right">
+                        <div className="text">
+                            <p>
+                                We're Jack & Zak, childhood friends and co-founders of Safety Straw.
+                            </p>
+                            <p>
+                                The company was born to create a safer environment for all and make a real difference. Want to find out more about our story?
+                            </p>
+                        </div>
+                        <div className="contact">
+                            <button>Contact Us</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-          
-        
         </div>
     );
     }
