4111515fda6aaf71dc3c869d08753bff4f8a798e
reitak97 <reita.koyama97@gmail.com>
2024-10-15 17:09:45 -0400

    restored app.jsx to original

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from "react";
 import "./App.css";
-import Video from "./components/video";
-
-
+import Navbar from "./components/nav.jsx";
+import Video from "./components/video.jsx";
+import Footer from "./components/footer.jsx"
+import Background from "./components/background.jsx"
+import Mission from "./components/our_mission.jsx"
+import Product from "./components/product.jsx"
 function App() {
   return(
+    <div className="app">
+    
+    <Navbar/>
     <Video/>
+    <Product></Product>
+    <Background></Background>
+    <Mission></Mission>
+    <Footer/>
+  </div>
+
   )
 
 }
