8a934c887d6e835f55ec819647885df5420a4310
Dmitry Starodubtsev <starodu5@gmail.com>
2024-10-20 22:48:10 -0400

    our mission responsive + mobile

--- frontend/src/components/our_mission.jsx
+++ frontend/src/components/our_mission.jsx
@@ -7,9 +7,11 @@ export default function Our_mission() {
   return (
     <div className="our_mission">
       <div className="our_mission-whitebox">
+        <div class="content-wrapper">
+          <p className="our_mission-p">OUR MISSION</p>
+          <h1 className="our_mission-text">Safety Straw’s mission is to eliminate the number of date rape victims from the world. Our goal is to create a <span className="text-color">safer</span>, more <span className="text-color">secure</span> nightlife environment.”</h1>
+        </div>
         <img className="whitebox-quotation" src={quotation} alt="" />
-        <p className="our_mission-p">OUR MISSION</p>
-        <h1 className="our_mission-text">Safety Straw’s mission is to eliminate the number of <br />date rape victims from the world. Our goal is to <br /> create a <span className="text-color">safer</span>, more <span className="text-color">secure</span> nightlife environment.”</h1>
         <img className="jack_and_zack" src={jack_and_zack} alt="" />
       </div>
     </div>
