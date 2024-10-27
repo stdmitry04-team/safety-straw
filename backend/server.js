17d9ef065f384593bbbf0a53fac365e4deda04a4
Dmitry Starodubtsev <starodu5@gmail.com>
2024-10-27 00:19:14 -0400

    minor fixes

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ require("dotenv").config({ path: "./config.env" });
 require("dotenv").config({ path: "./database.env" });
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;  //in deployment process.env.PORT should be 3000
+const baseUrl = process.env.BASE_URL || `http://localhost:${PORT}`;
 
 const crypto = require("crypto");
 const nodemailer = require("nodemailer");
@@ -156,12 +157,13 @@ app.post("/api/waitlist", async (req, res) => {
         pass: process.env.EMAIL_PASS,
       },
     });
-
+    
+    const verificationLink = `${baseUrl}/api/waitlist/confirm?token=${token}`;
     let mailOptions = {
       from: process.env.EMAIL_USER,
       to: email,
       subject: "Safety Straw Waitlist Email Verification",
-      html: `<p>Please verify your email by clicking <a href="http://localhost:5000/api/waitlist/confirm?token=${token}">here</a>.</p>`,
+      html: `<p>Please verify your email by clicking <a href="${verificationLink}">here</a>.</p>`
     };
 
     transporter.sendMail(mailOptions);
@@ -191,6 +193,17 @@ app.get("/api/waitlist/confirm", async (req, res) => {
     res.status(500).json({ message: "Failed to find user" });
   }
   res.status(200).send("Your email has been successfully verified!");
+});
+
+// Serve static files from the public directory (serves the built react files in deployment)
+app.use(express.static('public'));
+
+// Handle React routing, return all other requests to React app
+app.get('*', (req, res) => {
+    // exclude all routes that begin with '/api/', those will be handled by express.js
+    if (!req.path.startsWith('/api/')) {
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    }
 });
 
 app.listen(PORT, async () => {});
