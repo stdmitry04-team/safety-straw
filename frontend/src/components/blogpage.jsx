441666e1a8b2893ebe13a4c7a34479428dd1fd49
emberzhao1 <zhaoembe@msu.edu>
2024-10-31 17:44:25 -0400

    renaming class names

--- frontend/src/components/blogpage.jsx
+++ frontend/src/components/blogpage.jsx
@@ -2,15 +2,19 @@ import BlogPost from "./blogpost"
 import blog_l_image from "../images/blogimageleft.png"
 import blog_r_image from "../images/blogimageright.png"
 import carrot_2 from "../assets/blogcarrot2.svg"
+import blog_page_image from "../assets/blogpagemainimg.svg"
 import "../styles/blogpage.css";
 
 export function BlogPage() {
     return (
-        <div className = "blog-page">
-            <div className="side-bar-posts">
-                <div className = "right-title">
+        <div className = "blog_page">
+            <div className = "blog_page_left">
+                <img className = "blog_page_image" src={blog_page_image} alt="" />
+            </div>
+            <div className="sidebar_posts">
+                <div className = "right_title">
                     <img className = "carrot_2" src={carrot_2} alt="" />
-                    <p className = "carrot-label"> Latest Blogs</p>  
+                    <p className = "carrot_label"> Latest Blogs</p>  
                 </div>     
                 <BlogPost imageSrc={blog_l_image} title={ <>How Safety Straw Products Are <br /> Environmental Friendly </>}  date='Aug 20, 2024' />
                 <BlogPost imageSrc={blog_r_image} title={ <>How the Safety Straw Straws <br />Changed My Life</>} date='Jul 1, 2024' />
