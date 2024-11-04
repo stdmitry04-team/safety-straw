5985e69d9e95e5330661e6f2ce7c348ae68ce80e
emberzhao1 <zhaoembe@msu.edu>
2024-11-03 19:12:33 -0500

    working on transformation

--- frontend/src/components/blogpage.jsx
+++ frontend/src/components/blogpage.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import BlogPost from "./blogpost";
 import blog_l_image from "../images/blogimageleft.png";
 import blog_r_image from "../images/blogimageright.png";
@@ -6,37 +7,38 @@ import carrot from "../assets/blogcarrot1.svg";
 import blog_page_image from "../images/blogpagemainimg.png";
 import blog_page_article from "../assets/blogpage-article.svg";
 import "../styles/blogpage.css";
-import React, { useState } from "react";
 
 export function BlogPage() {
     const [shifted, setShifted] = useState(false);
 
     const shiftLeft = () => {
-        setShifted(prev => !prev); // Toggle the shifted class
+        setShifted(prevShifted => !prevShifted); // Toggle the shifted state
     };
+    
 
     return (
         <div className={`blog_page ${shifted ? 'shifted' : ''}`}>
             <div className="blog_page_left">
                 <img
                     className="blog_page_image"
-                    onClick={shiftLeft} // Corrected onClick syntax
                     src={blog_page_image}
                     alt=""
                 />
                 <img className="blog_page_article" src={blog_page_article} alt="" />
             </div>
             <div className="sidebar_posts">
-                <img className="carrot" src={carrot} alt="" />
+                <img className={`carrot ${shifted ? 'shifted' : ''}`} src={carrot} alt="" onClick={shiftLeft}  />
                 <div className="right_title">
-                    <img className="carrot_2" src={carrot_2} alt="" />
-                    <p className="carrot_label"> Latest Blogs</p>
+                    <img className={`carrot_2 ${shifted ? 'shifted' : ''}`} src={carrot_2} alt="" />
+                    <p className="carrot_label">Latest Blogs</p>
                 </div>
-                <BlogPost imageSrc={blog_l_image} title={<>How Safety Straw Products Are <br /> Environmental Friendly </>} date="Aug 20, 2024" />
-                <BlogPost imageSrc={blog_r_image} title={<>How the Safety Straw Straws <br />Changed My Life</>} date="Jul 1, 2024" />
-                <BlogPost imageSrc={blog_r_image} title={<>How the Safety Straw Straws <br />Changed My Life</>} date="Jul 1, 2024" />
+                <BlogPost imageSrc={blog_l_image} title={<>How Safety Straw Products Are <br /> Environmental Friendly </>} date='Aug 20, 2024' />
+                <BlogPost imageSrc={blog_r_image} title={<>How the Safety Straw Straws <br />Changed My Life</>} date='Jul 1, 2024' />
+                <BlogPost imageSrc={blog_r_image} title={<>How the Safety Straw Straws <br />Changed My Life</>} date='Jul 1, 2024' />
             </div>
+            <p id="demo"></p>
         </div>
+        
     );
 }
 
