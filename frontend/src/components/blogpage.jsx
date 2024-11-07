329003ee05a4be580ef1e4bb22572790e55a0d4b
emberzhao1 <zhaoembe@msu.edu>
2024-11-06 22:00:59 -0500

    worked on latest blogs button responsiveness and started on media queries for mobile

--- frontend/src/components/blogpage.jsx
+++ frontend/src/components/blogpage.jsx
@@ -17,26 +17,34 @@ export function BlogPage() {
     
 
     return (
-        <div className={`blog_page ${shifted ? 'shifted' : ''}`}>
-            <div className="blog_page_left">
-                <img
-                    className="blog_page_image"
-                    src={blog_page_image}
-                    alt=""
-                />
-                <img className="blog_page_article" src={blog_page_article} alt="" />
-            </div>
-            <div className="sidebar_posts">
-                <img className={`carrot ${shifted ? 'shifted' : ''}`} src={carrot} alt="" onClick={shiftLeft}  />
-                <div className="right_title">
-                    <img className={`carrot_2 ${shifted ? 'shifted' : ''}`} src={carrot_2} alt="" />
-                    <p className="carrot_label">Latest Blogs</p>
+        <div>
+            <body className="blog_page_body">
+            <div className={`blog_page ${shifted ? 'shifted' : ''}`}>
+                <div className="blog_page_left">
+                    <img
+                        className="blog_page_image"
+                        src={blog_page_image}
+                        alt=""
+                    />
+                    <img className="blog_page_article" src={blog_page_article} alt="" />
                 </div>
-                <BlogPost imageSrc={blog_l_image} title={<>How Safety Straw Products Are <br /> Environmental Friendly </>} date='Aug 20, 2024' />
-                <BlogPost imageSrc={blog_r_image} title={<>How the Safety Straw Straws <br />Changed My Life</>} date='Jul 1, 2024' />
-                <BlogPost imageSrc={blog_r_image} title={<>How the Safety Straw Straws <br />Changed My Life</>} date='Jul 1, 2024' />
-            </div>
-            <p id="demo"></p>
+                
+                <div className="sidebar_posts">
+                    <img className={`carrot ${shifted ? 'shifted' : ''}`} src={carrot} alt="" onClick={shiftLeft}  />
+                    <div className = {`side_bar_imgs ${shifted ? 'shifted' : ''}`}>
+                        <div className="right_title">
+                            <img className={`carrot_2 ${shifted ? 'shifted' : ''}`} src={carrot_2} alt=""onClick={shiftLeft} />
+                            <p className="carrot_label">Latest Blogs</p>
+                        </div>
+                        <BlogPost imageSrc={blog_l_image} title={<>How Safety Straw Products Are <br /> Environmental Friendly </>} date='Aug 20, 2024' />
+                        <BlogPost imageSrc={blog_r_image} title={<>How the Safety Straw Straws <br />Changed My Life</>} date='Jul 1, 2024' />
+                        <BlogPost imageSrc={blog_r_image} title={<>How the Safety Straw Straws <br />Changed My Life</>} date='Jul 1, 2024' />
+                    </div>
+                    
+                </div>
+                </div>
+            </body>
+
         </div>
         
     );
