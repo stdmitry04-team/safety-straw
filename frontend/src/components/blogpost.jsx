b57b7a3f98e96da9fd5bcd26b738be50db97d163
Shiyuan Chen <chens100@msu.edu>
2024-11-07 20:41:45 -0500

    mobile view

--- frontend/src/components/blogpost.jsx
+++ frontend/src/components/blogpost.jsx
@@ -1,13 +1,29 @@
+// import React from 'react';
+// import "../styles/blogposts.css";
+
+
+// export default function BlogPost({ imageSrc, title, date, className }) {
+//     return (
+//         <div className={`blog-post-item ${className}`}>
+//             <img src={imageSrc} alt={title} className="blog-post-image" />
+//             <p className="blog-post-date">{date}</p>
+//             <h3 className="blog-post-title">{title}</h3>
+//         </div>
+//     );
+// }
+
+
 import React from 'react';
 import "../styles/blogposts.css";
-
 
 export default function BlogPost({ imageSrc, title, date, className }) {
     return (
         <div className={`blog-post-item ${className}`}>
             <img src={imageSrc} alt={title} className="blog-post-image" />
-            <p className="blog-post-date">{date}</p>
-            <h3 className="blog-post-title">{title}</h3>
+            <div className="blog-post-content">
+                <p className="blog-post-date">{date}</p>  
+                <h3 className="blog-post-title">{title}</h3> 
+            </div>
         </div>
     );
 }
