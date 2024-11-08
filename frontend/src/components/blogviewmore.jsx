1c4f30b3bd82f61c9b0cc948079f6f148d7273bc
Shiyuan Chen <chens100@msu.edu>
2024-11-08 16:59:53 -0500

    navbar import, fixed desktop layout

--- frontend/src/components/blogviewmore.jsx
+++ frontend/src/components/blogviewmore.jsx
@@ -1,29 +1,3 @@
-// import React from 'react';
-// import "../styles/blogviewmore.css"
-// import BlogPost from "./blogpost";
-// import blog_l_image from "../images/blogimageleft.png";
-// import blog_m_image from "../images/blogimagemiddle.png";
-// import blog_r_image from "../images/blogimageright.png";
-// import blog_middown_image from "../images/strawtint.png"; 
-// import blog_rdown_image from "../images/bartint.png"; 
-
-// export default function BlogViewMore() {
-//     return (
-//         <div className="blog-view-more">    
-//             <p className="blog-view-header">BLOG POSTS</p>
-//             <div className="posts">
-//                 <BlogPost imageSrc={blog_l_image} title={ <>How Safety Straw Products Are <br /> Environmental Friendly </>}  date='Aug 20, 2024' />
-//                 <BlogPost imageSrc={blog_m_image} title={ <>How Do Bars Use Safety Straw <br />Products?</>} date='Aug 23, 2024' />
-//                 <BlogPost imageSrc={blog_r_image} title={ <>How the Safety Straw Straws <br />Changed My Life</>} date='Jul 1, 2024' />
-
-//                 <BlogPost imageSrc={blog_r_image} title={ <>How Safety Straw Products Are <br /> Environmental Friendly </>}  date='Aug 20, 2024' />
-//                 <BlogPost imageSrc={blog_middown_image} title={ <>How Do Bars Use Safety Straw <br />Products?</>} date='Aug 23, 2024' />
-//                 <BlogPost imageSrc={blog_rdown_image} title={ <>How the Safety Straw Straws <br />Changed My Life</>} date='Jul 1, 2024' />
-//             </div>
-//         </div>
-//     );
-// }
-
 import React, { useEffect, useState } from 'react';
 import "../styles/blogviewmore.css";
 import BlogPost from "./blogpost";
@@ -33,6 +7,9 @@ import blog_r_image from "../images/blogimageright.png";
 import blog_middown_image from "../images/strawtint.png"; 
 import blog_rdown_image from "../images/bartint.png"; 
 import blogpagebar from "../images/blogpagebar.png"; // Import the pagination bar image
+import Navbar from "./nav.jsx";
+import WaitlistBar from "./WaitlistBar.jsx";
+import Footer from "./footer.jsx";
 
 export default function BlogViewMore() {
     const [isMobile, setIsMobile] = useState(false);
@@ -45,7 +22,9 @@ export default function BlogViewMore() {
     }, []);
 
     return (
-        <div className="blog-view-more">    
+        <div>
+            <Navbar /> 
+            <div className="blog-view-more">    
             <p className="blog-view-header">BLOG POSTS</p>
             <div className="blog-view-more-posts">
                 {isMobile ? (
@@ -71,6 +50,7 @@ export default function BlogViewMore() {
                     <img src={blogpagebar} alt="Pagination" className="pagination-image" />
                 </div>
             )}
+            </div>
         </div>
     );
 }
