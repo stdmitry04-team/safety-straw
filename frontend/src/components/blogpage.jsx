8fe0d37a42f7e7ffec412af6fa08721f5e2d03b3
emberzhao1 <zhaoembe@msu.edu>
2024-11-08 20:55:40 -0500

    working on responsive design

--- frontend/src/components/blogpage.jsx
+++ frontend/src/components/blogpage.jsx
@@ -5,7 +5,9 @@ import blog_m_image from "../images/blog-indv-page-img-middle.png";
 import carrot_2 from "../assets/blogcarrot2.svg";
 import carrot from "../assets/blogcarrot1.svg";
 import blog_page_image from "../images/blogpagemainimg.png";
+import blog_page_image_mobile from "../images/blogpage-main-img-mobile.png";
 import blog_page_article from "../assets/blogpage-article.svg";
+import blog_page_article_mobile from "../assets/blogpage-article-mobile.svg";
 import Navbar from "./nav.jsx";
 import WaitlistBar from "./WaitlistBar.jsx";
 import Footer from "./footer.jsx";
@@ -26,7 +28,9 @@ export function BlogPage() {
             <div className={`blog_page ${shifted ? 'shifted' : ''}`}>
                 <div className="blog_page_left">
                     <img className="blog_page_image" src={blog_page_image} alt=""/>
+                    <img className="blog_page_image_mobile" src={blog_page_image_mobile} alt=""/>
                     <img className="blog_page_article" src={blog_page_article} alt="" />
+                    <img className="blog_page_article_mobile" src={blog_page_article_mobile} alt="" />
                 </div>
                 
                 <div className={`sidebar_posts`}>
